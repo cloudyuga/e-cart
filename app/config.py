acb0cab3e23ede2951ad41dcbdbd8da0a740b1e0
peterfebin <peterfebin@gmail.com>
2017-07-21 10:28:31 +0530

    modified mysql credentials

--- app/config.py
+++ app/config.py
@@ -1,10 +1,10 @@
 from app import app
 from flask_mysqldb import MySQL
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '1234'
-app.config['MYSQL_DB'] = 'ecommerce'
+app.config['MYSQL_HOST'] = 'sql12.freemysqlhosting.net'
+app.config['MYSQL_USER'] = 'sql12186197'
+app.config['MYSQL_PASSWORD'] = 'v1LbHD2jDU'
+app.config['MYSQL_DB'] = 'sql12186197'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 mysql = MySQL(app)
