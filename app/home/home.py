7c3a748daf774a71940413f260970bfd40aad077
peterfebin <peterfebin@gmail.com>
2017-10-13 13:58:05 +0530

    created yaml file to store URL

--- app/home/home.py
+++ app/home/home.py
@@ -6,6 +6,8 @@ import logging
 import jwt
 import requests
 import json
+import yaml
+import os
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -20,13 +22,16 @@ def index():
 
 @app.route('/payment/<int:id>')
 def payment(id):
+    directoryPath = os.path.dirname(os.path.realpath(__file__))
     logger.info("Entered payment method")
+    with open("%s/../config.yaml" % directoryPath, 'r') as stream:
+         paymentUrl = yaml.load(stream)['paymentUrl']
     orderId = id
     logger.info("Generating token")
     token = jwt.encode({}, app.config['SECRET_KEY'])
     token = token.decode('UTF-8')
     headers = {'access-token': token, 'content-type': 'application/json'}
-    url = 'http://payment:5005/payment'
+    url = paymentUrl
     data = {"orderId": orderId}
     data = json.dumps(data)
     response = requests.post(url, data=data, headers=headers)
